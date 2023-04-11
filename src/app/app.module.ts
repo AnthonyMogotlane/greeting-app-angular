9623832226c7f1aca89a637ea454f2fcda975984
Anthony Mogotlane <almogotlane@gmail.com>
2023-04-11 07:33:42 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { GreetedComponent } from './greeted/greeted.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GreetedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "home", component: HomeComponent },
+      { path: "greeted", component: GreetedComponent },
+      { path: " ", redirectTo: "home" },
+      { path: "**", redirectTo: "home" }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/greeted/greeted.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPerson } from './person';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GreetedService {
+
+  private greetedNamesUrl: string = "https://localhost:7088/greeted";
+
+  constructor(private http: HttpClient) { }
+
+  getGreetedNames(): Observable<IPerson[]> {
+    return this.http.get<IPerson[]>(this.greetedNamesUrl).pipe(
+      tap(data => console.log("All", JSON.stringify(data))),
+      catchError(this.handleError)
+    )
+  }
+
+  // Handling error
+  private handleError(err: HttpErrorResponse) {
+    let errMsg = "";
+    if(err.error instanceof ErrorEvent)
+      errMsg = "An error occured: " + err.error.message;
+    else
+      errMsg = `Server returned code: ${err.status}, error message is: ${err.message}` 
+    
+    console.log(errMsg);
+    return throwError(() => errMsg);
+  }
+}
+
