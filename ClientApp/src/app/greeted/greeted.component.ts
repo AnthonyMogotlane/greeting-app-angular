49d424c5c7856d590193c9cc662fcc422cd14178
Anthony Mogotlane <almogotlane@gmail.com>
2023-04-19 16:31:08 +0200

    Added deletedName method

--- ClientApp/src/app/greeted/greeted.component.ts
+++ ClientApp/src/app/greeted/greeted.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GreetedService } from './greeted.service';
 import { IPerson } from './person';
 
+
 @Component({
   templateUrl: './greeted.component.html',
   styleUrls: ['./greeted.component.css']
@@ -9,15 +10,30 @@ import { IPerson } from './person';
 export class GreetedComponent implements OnInit {
   pageTitle: string = "Greeted Names";
   errMsg: string = "";
-
+  
   greetedNames: IPerson[] = [];
-
+  // Table properties
+  displayedColumns: string[] = ['name', 'count', 'deleteBtns'];
+  dataSource!: IPerson[];
+  
   constructor(private greetedService: GreetedService) {}
 
   ngOnInit(): void {
     this.greetedService.getGreetedNames().subscribe({
-      next: greetedNames => this.greetedNames = greetedNames,
+      next: greetedNames => {
+        this.greetedNames = greetedNames;
+        this.dataSource = greetedNames;
+      },
       error: err => this.errMsg = err
+    })
+  }
+
+  onDeleteName(id: number){
+    this.greetedService.deleteName(id).subscribe({
+      next: greetedNames => {
+        this.greetedNames = greetedNames;
+        this.dataSource = this.greetedNames;
+      }
     })
   }
 }
